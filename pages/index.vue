f8fc189c2b512db555ca3dbae21dff3746e81a64
FELIPPE MENILE <111753437+fefebueno@users.noreply.github.com>
2022-09-05 11:41:41 +0000

    alterações

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,16 @@
 <template>
   <section class="section">
-    <h1 class="title">Página inicial</h1>
-  </section>
-</template>
-
+    
+  <!-- carousel com imagens -->
+    <b-carousel>
+      <b-carousel-item>
+        <b-image class="image" src="https://picsum.photos/id/431/1230/300"></b-image>
+      </b-carousel-item>
+      <b-carousel-item>
+        <b-image class="image" src="https://picsum.photos/id/432/1230/300"></b-image>
+      </b-carousel-item>      
+      <b-carousel-item>
+        <b-image class="image" src="https://picsum.photos/id/433/1230/300"></b-image>
+      </b-carousel-item>            
+    </b-carousel> 
+    
